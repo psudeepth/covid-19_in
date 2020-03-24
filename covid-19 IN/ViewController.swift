a8083fca089a5be3804a2de8ce57a8b2850d1b1e
Sudeepth Patinjarayil <psudeepth@gmail.com>
2020-03-24 15:52:23 +0530

    Initial Commit

---
+++ covid-19 IN/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  covid-19 IN
+//
+//  Created by Deepthu on 24/03/20.
+//  Copyright © 2020 sudeepthpatinjarayil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
